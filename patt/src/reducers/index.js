f44e50b440a2aaebdcc33be16641c47e1bfc2a07
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-05-20 13:26:37 -0700

    Built test reducer

---
+++ patt/src/reducers/index.js
@@ -0,0 +1,13 @@
+const initialState = {
+    test: "test"
+}
+
+
+const rootReducer = (state = initialState, action) => {
+    switch (action.type) {
+        default: return state; 
+    } 
+} 
+
+export default rootReducer; 
+
