84f20192068b1f9f5853d973e5ade9d73543080d
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-05-20 13:21:25 -0700

    Boilerplated index.js file

--- patt/src/index.js
+++ patt/src/index.js
@@ -2,6 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Importing extra Redux dependencies
+import { createStore, applyMiddleware, compose } from 'redux'; 
+import { Provider }  from 'react-redux'; 
+import thunk from 'redux-thunk'; 
+import logger from 'redux-logger'; 
+
+import rootReducer from '../src/reducers'; 
+
+// Creating compose enhancer 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; 
+
+// Creating store
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk, logger))); 
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 
