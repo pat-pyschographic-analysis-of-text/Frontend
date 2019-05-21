7d1f2e610df31dd66973e2f3a5e3fe33af0dcee4
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-05-21 08:44:30 -0700

    Added prelim styling to DataCard page

--- patt/src/pages/DataCardPage.js
+++ patt/src/pages/DataCardPage.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { DataCard, SettingsButton } from "../components";
-import { Link } from "react-router-dom";
 
 class DataCardPage extends React.Component {
   render() {
@@ -8,9 +7,6 @@ class DataCardPage extends React.Component {
       <>
         <SettingsButton /> 
         <DataCard />
-        <Link to="/search">
-          <button>Search again</button>
-        </Link>
       </>
     );
   }
