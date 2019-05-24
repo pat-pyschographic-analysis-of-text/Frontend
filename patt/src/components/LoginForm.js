542a6d52013bb3814d567afd9c807bb4d197bb46
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-05-23 17:15:16 -0700

    RemovingLink from imports

--- patt/src/components/LoginForm.js
+++ patt/src/components/LoginForm.js
@@ -1,5 +1,4 @@
 import React from 'react'; 
-import { Link } from 'react-router-dom'; 
 import { connect } from 'react-redux'
 import { login } from '../actions'
 import { withRouter } from 'react-router-dom' 
