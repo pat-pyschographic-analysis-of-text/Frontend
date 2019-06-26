88589f85cd10ee6193684ef2120613cc2573f5a7
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-06-25 19:06:35 -0700

    Added comments about functionality to PrivateRoute

--- patt/src/PrivateRoute.js
+++ patt/src/PrivateRoute.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Route, Redirect } from 'react-router-dom'
 
+// PrivateRoute checks localStorage for a token, and renders a Component if a token exists. 
+// If there is no token, it redirects our user to the login page at "/" 
+
 const PrivateRoute = ({ component: Component, ...rest}) => {
     return (
         <Route 
